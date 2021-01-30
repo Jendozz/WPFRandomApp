20d37e90e67a9681f24c433faacd453bd6333981
n_zhenek@mail.ru <n_zhenek@mail.ru>
2021-01-30 17:24:04 +0400

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFRandomThings.Model;
+
+namespace WPFRandomThings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BindingList<Message> RandomList;
+       
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Window_Loaded (object sender, RoutedEventArgs e)
+        {
+           
+            RandomList = new BindingList<Message>();
+
+            ListText.DataContext = RandomList;
+
+            ButtonAddToList.Click += ButtonAddToList_Click;
+
+            ButtonRemoveFromList.Click += ButtonRemoveFromList_Click;
+
+            ButtonRandom.Click += ButtonRandom_Click;
+            
+            RandomList.ListChanged += ListChange;
+
+
+        }
+
+
+
+        private void ButtonAddToList_Click(object sender, RoutedEventArgs e)
+        {
+           
+           
+            if (inputText.Text.Length > 0) 
+            {
+                Message message = new Message(inputText.Text);
+                RandomList.Add(message);
+                inputText.Clear();
+               
+            }
+            else
+            {
+                
+                TextMessages.Text = "Вы не ввели значение";
+              
+            }
+            e.Handled = true;
+        }
+
+        private void ListChange(object sender, ListChangedEventArgs e)
+        {
+            ListText.Clear();
+            foreach (Message Item in RandomList)
+            {
+                ListText.Text += $" {Item.Mes}\n";
+            }
+           
+        }
+
+        private void ButtonRemoveFromList_Click(object sender, RoutedEventArgs e)
+        {
+             var item = RandomList.LastOrDefault();
+            RandomList.Remove(item);
+            e.Handled = true;
+        }
+
+        private void ButtonRandom_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            var MaxSizeList = RandomList.Count;
+
+            var result = rnd.Next(0, MaxSizeList);
+
+            TextMessages.Text = $"Ваше рандомное значеие - {RandomList[result].Mes}";
+
+            e.Handled = true;
+        }
+    }
+}
---
+++ Model/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFRandomThings.Model
+{
+    class Message
+    {
+        public string Mes { get; set; }
+
+        public Message(string _mes)
+        {
+            Mes = _mes;
+        }
+
+
+    }
+    
+}
